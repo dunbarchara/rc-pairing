9c2246b9a3d466c9a547f369e231ef479ff25dee
dunbarchara <230891692+dunbarchara@users.noreply.github.com>
2026-08-16 17:02:25 -0400

    Finished RC Programming tasks: - Implement collision - Spawn Zombies - Make Zombies follow and overtake player - Add fire weapon mechanic to kill zombies

--- game.ts
+++ game.ts
@@ -3,12 +3,18 @@ import { stdin as input, stdout as output } from 'node:process';
 import * as fs from 'fs';
 import * as path from 'path';
 
+import { execSync } from 'child_process';
+
+function nodeSyncSleep(ms: number): void {
+    execSync(`node -e "setTimeout(() => {}, ${ms})"`);
+}
+
 declare const process: any; // clear red squiggle on process references, we're running ts file directly node v24 so just cosmetic in editor
 
 /*
 =================================================================================
 
-    CLASS DEFS: Map, Asset, Canvas
+    CLASS DEFS: Game, Asset, Canvas
     
 =================================================================================
 */
@@ -37,15 +43,23 @@ class Asset {
     }
 }
 
-class Map {
-    map: string[][]; // map of game represented by chars
+class Game {
+    map: string[][]; // game of game represented by chars
+    availableSpaces: boolean[][];
     viewboxRows: number; // sizing for viewbox
     viewboxCols: number; // sizing for viewbox
     renderRowOffset: number; // for convenience, half the size of viewbox
     renderColOffset: number; // for convenience, half the size of viewbox
+    characterPosition: Position;
+    zombiePositions: Position[];
+    logStatement: string = '';
+    gameOver: boolean = false;
+    gameWon: boolean = false;
 
     constructor(rows: number, cols: number, viewboxRows: number, viewboxCols: number) {
-        this.map = Array.from({ length: rows }, () => Array.from({ length: cols }, () => ' ')); // init empty map of spaces
+        this.map = Array.from({ length: rows }, () => Array.from({ length: cols }, () => ' ')); // init empty game of spaces
+        this.availableSpaces = Array.from({ length: rows }, () => Array.from({ length: cols }, () => true));
+
         for (let i = 0; i < this.map.length; i++) {
             for (let j = 0; j < this.map[0].length; j++) {
                 if (i === 0 ||
@@ -53,7 +67,8 @@ class Map {
                     j === 0 ||
                     j === this.map[0].length - 1
                 ) {
-                    this.map[i][j] = '#'; // init map border of '#'
+                    this.map[i][j] = '#'; // init game border of '#'
+                    this.availableSpaces[i][j] = false;
                 }
             }
         }
@@ -62,42 +77,46 @@ class Map {
         this.viewboxCols = viewboxCols;
         this.renderRowOffset = Math.floor(this.viewboxRows / 2);
         this.renderColOffset = Math.floor(this.viewboxCols / 2);
+        this.characterPosition = { row: Math.floor(this.map.length / 2), col: Math.floor(this.map[0].length / 2) };
+        this.availableSpaces[this.characterPosition.row][this.characterPosition.col] = false;
+        this.zombiePositions = [];
     }
 
     addAsset(asset: Asset, position: Position) {
-        // place the asset at the specified position by copying its contents into the map[][] def
+        // place the asset at the specified position by copying its contents into the game[][] def
         // position is top left anchor
         for (let i = 0; i < asset.asset.length; i++) {
             for (let j = 0; j < asset.asset[0].length; j++) {
                 if ((position.row + i) < 0 || (position.row + i) >= this.map.length ||
                     (position.col + j) < 0 || (position.col + j) >= this.map[0].length) {
-                    // out of map[][] bounds, throw error, only support full asset in bounds
+                    // out of game[][] bounds, throw error, only support full asset in bounds
                     throw `ERROR: addAsset — invalid asset position ${asset.assetPath} ROW: ${position.row + i} COL: ${position.col + j}`;
                 }
 
                 this.map[position.row + i][position.col + j] = asset.asset[i][j];
+                this.availableSpaces[position.row + i][position.col + j] = false;
             }
         }
     }
 
-    renderViewbox(position: Position): string {
-        // return a string rendering of the map that fits within the viewbox centered around the specified position
+    renderViewbox(): string {
+        // return a string rendering of the game that fits within the viewbox centered around the specified position
 
-        const startRow = position.row - this.renderRowOffset; // row pos that starts viewbox
-        const startCol = position.col - this.renderColOffset; // col pos that starts the viewbox
+        const startRow = this.characterPosition.row - this.renderRowOffset; // row pos that starts viewbox
+        const startCol = this.characterPosition.col - this.renderColOffset; // col pos that starts the viewbox
 
         let renderBox = '_'.repeat(this.viewboxCols + 2) + '\n'; // upper viewbox border
 
         for (let i = startRow; i < startRow + this.viewboxRows; i++) {
             renderBox += '|'; // lefthand viewbox border
             for (let j = startCol; j < startCol + this.viewboxCols; j++) {
-                if (i === position.row && j === position.col) {
-                    renderBox += 'C'; // render the 'character' in center of viewbox
+                if (i === this.characterPosition.row && j === this.characterPosition.col) {
+                    renderBox += this.gameOver ? 'Z' : 'C'; // render the 'character' in center of viewbox, or 'Z' if gameover
                 } else if (i < 0 || i >= this.map.length || j < 0 || j >= this.map[0].length) {
                     // out of bounds, render empty space
                     renderBox += ' ';
                 } else {
-                    // in bounds, render the map
+                    // in bounds, render the game
                     renderBox += this.map[i][j];
                 }
             }
@@ -109,6 +128,140 @@ class Map {
 
         return renderBox;
     }
+
+    canMove(position: Position): boolean {
+        this.logStatement = `trying to move ${position.row}, ${position.col} -> ${this.map[position.row][position.col] === ' '}`;
+        return this.map[position.row][position.col] === ' ';
+
+    }
+
+    spawnZombies(num: number) {
+        for (let i = 0; i < num; i++) {
+            // for each zombie
+
+            let pos = this.getRandomPosition();
+            while (this.availableSpaces[pos.row][pos.col] === false) {
+                pos = this.getRandomPosition();
+            }
+
+            this.zombiePositions.push(pos);
+            this.map[pos.row][pos.col] = 'Z';
+            this.availableSpaces[pos.row][pos.col] = false;
+        }
+    }
+
+    getRandomPosition(): Position {
+        return { row: Math.floor(Math.random() * this.availableSpaces.length), col: Math.floor(Math.random() * this.availableSpaces[0].length) };
+    }
+
+    iterateZombies() {
+        for (let i = 0; i < this.zombiePositions.length; i++) {
+            // Zombie logic is not intelligent, they will simply check WASD directions and pick the first one
+            // that is a movable space closer to the player position
+            // no path finding, so zombies can definitely get stuck on the opposite sides of structures
+
+            const zPos = this.zombiePositions[i];
+
+            let newPos: Position = { row: zPos.row, col: zPos.col };
+
+            if (zPos.row > this.characterPosition.row &&
+                this.canMove(this.getNewPosition('w', zPos))) {
+                // move up
+                newPos = this.getNewPosition('w', zPos);
+            } else if (zPos.col > this.characterPosition.col &&
+                this.canMove(this.getNewPosition('a', zPos))) {
+                // move left
+                newPos = this.getNewPosition('a', zPos);
+            } else if (zPos.row < this.characterPosition.row &&
+                this.canMove(this.getNewPosition('s', zPos))) {
+                // move down
+                newPos = this.getNewPosition('s', zPos);
+            } else if (zPos.col < this.characterPosition.col &&
+                this.canMove(this.getNewPosition('d', zPos))) {
+                // move right
+                newPos = this.getNewPosition('d', zPos);
+            }
+
+
+            this.map[zPos.row][zPos.col] = ' '; // free up
+            this.map[newPos.row][newPos.col] = 'Z'; // free up
+            this.zombiePositions[i] = newPos;
+
+            // check if game over
+            if (newPos.row === this.characterPosition.row && newPos.col === this.characterPosition.col) {
+                this.gameOver = true;
+                return;
+            }
+        }
+    }
+
+    getNewPosition(direction: string, startPos: Position): Position {
+        // given a star position and a direction, return the Position that represents a move in that direction
+        let rowInc = 0;
+        let colInc = 0;
+        switch (direction) {
+            case 'w':
+                rowInc = -1;
+                break;
+            case 'a':
+                colInc = -1;
+                break;
+            case 's':
+                rowInc = 1;
+                break;
+            case 'd':
+                colInc = 1;
+                break;
+            default:
+                throw 'Invalid move input';
+        }
+
+        return { row: startPos.row + rowInc, col: startPos.col + colInc };
+    }
+
+    tryMoveCharacter(direction: string) {
+        // try to move the character in the given direction
+        const newPosition = this.getNewPosition(direction, this.characterPosition);
+
+        if (this.canMove(newPosition)) {
+            this.characterPosition = newPosition;
+        }
+    }
+
+    fireBullet(direction: string) {
+        // fire a bullet within the view box, render each step until it hits a zombie, wall, or leaves viewbox
+        let bulletPosition = this.getNewPosition(direction, this.characterPosition);
+
+        // only shoot within viewbox
+        let fireDistance = (direction === 'w' || direction === 's') ? this.renderRowOffset : this.renderColOffset;
+
+        while (this.canMove(bulletPosition) && fireDistance > 0) {
+            // paint frame
+            this.map[bulletPosition.row][bulletPosition.col] = '*';
+            repaintTerminal();
+            nodeSyncSleep(50);
+            this.map[bulletPosition.row][bulletPosition.col] = ' ';
+            bulletPosition = this.getNewPosition(direction, bulletPosition);
+            fireDistance--;
+        }
+
+        // bullet position is an occupied space, check if zombie
+        for (let i = 0; i < this.zombiePositions.length; i++) {
+            if (bulletPosition.row === this.zombiePositions[i].row &&
+                bulletPosition.col === this.zombiePositions[i].col) {
+                // zombie found, remove
+                this.map[bulletPosition.row][bulletPosition.col] = ' ';
+                this.zombiePositions.splice(i, 1);
+
+                if (this.zombiePositions.length === 0) {
+                    this.gameWon = true;
+                }
+                return;
+            }
+        }
+    }
+
+
 }
 
 
@@ -128,8 +281,8 @@ class Map {
 =================================================================================
 */
 
-//const map: Map = new Map(50, 150, 55, 200); // large viewbox (debugging)
-const map: Map = new Map(50, 150, 15, 40); // standard viewbox
+//const game: Game = new Game(50, 150, 55, 200); // large viewbox (debugging)
+const game: Game = new Game(50, 150, 15, 40); // standard viewbox
 
 // init assets
 const assetChurch: Asset = new Asset('./assets/building_church.txt');
@@ -141,26 +294,26 @@ const assetWindmill: Asset = new Asset('./assets/building_windmill.txt');
 const assetDuck: Asset = new Asset('./assets/duck.txt');
 
 // place assets
-map.addAsset(assetChurch, { row: 23, col: 120 });
-map.addAsset(assetHouse, { row: 18, col: 52 });
-map.addAsset(assetHouse, { row: 18, col: 86 });
-map.addAsset(assetHouse, { row: 28, col: 52 });
-map.addAsset(assetHouse, { row: 28, col: 86 });
-map.addAsset(assetHouse, { row: 35, col: 115 });
-map.addAsset(assetHouse, { row: 28, col: 86 });
-map.addAsset(assetMansion, { row: 20, col: 15 });
-map.addAsset(assetMansion, { row: 34, col: 18 });
-map.addAsset(assetLighthouse, { row: 5, col: 70 });
-map.addAsset(assetTotem, { row: 37, col: 60 });
-map.addAsset(assetTotem, { row: 37, col: 82 });
-map.addAsset(assetWindmill, { row: 3, col: 120 });
-map.addAsset(assetDuck, { row: 5, col: 8 });
-map.addAsset(assetDuck, { row: 7, col: 18 });
-map.addAsset(assetDuck, { row: 2, col: 22 });
-map.addAsset(assetDuck, { row: 4, col: 31 });
+game.addAsset(assetChurch, { row: 23, col: 120 });
+game.addAsset(assetHouse, { row: 18, col: 52 });
+game.addAsset(assetHouse, { row: 18, col: 86 });
+game.addAsset(assetHouse, { row: 28, col: 52 });
+game.addAsset(assetHouse, { row: 28, col: 86 });
+game.addAsset(assetHouse, { row: 35, col: 115 });
+game.addAsset(assetHouse, { row: 28, col: 86 });
+game.addAsset(assetMansion, { row: 20, col: 15 });
+game.addAsset(assetMansion, { row: 34, col: 18 });
+game.addAsset(assetLighthouse, { row: 5, col: 70 });
+game.addAsset(assetTotem, { row: 37, col: 60 });
+game.addAsset(assetTotem, { row: 37, col: 82 });
+game.addAsset(assetWindmill, { row: 3, col: 120 });
+game.addAsset(assetDuck, { row: 5, col: 8 });
+game.addAsset(assetDuck, { row: 7, col: 18 });
+game.addAsset(assetDuck, { row: 2, col: 22 });
+game.addAsset(assetDuck, { row: 4, col: 31 });
 
-// init character starting position in middle of the map
-let characterPosition: Position = { row: Math.floor(map.map.length / 2), col: Math.floor(map.map[0].length / 2) };
+// spawn zombies
+game.spawnZombies(4);
 
 
 
@@ -179,42 +332,46 @@ let characterPosition: Position = { row: Math.floor(map.map.length / 2), col: Ma
 =================================================================================
 */
 
+
+
 // Enable single keypress events from standard input
 readline.emitKeypressEvents(input);
 if (input.isTTY) {
     input.setRawMode(true);
 }
 
+let stepCounter = 0;
 let keyPressed = '';
+let firePhase = false;
 
 // Helper function to wait for a single keypress
 function waitForKeypress(): Promise<string> {
     return new Promise((resolve) => {
         const listener = (str: string, key: readline.Key) => {
             // Allow exiting via Ctrl+C
+            keyPressed = key.name;
             if (key.ctrl && key.name === 'c') {
                 process.exit(0);
             }
-            switch (key.name) {
-                case 'w':
-                    keyPressed = 'w';
-                    characterPosition.row -= 1;
-                    break;
-                case 'a':
-                    keyPressed = 'a';
-                    characterPosition.col -= 1;
-                    break;
-                case 's':
-                    keyPressed = 's';
-                    characterPosition.row += 1;
-                    break;
-                case 'd':
-                    keyPressed = 'd';
-                    characterPosition.col += 1;
-                    break;
-                default:
-                    break;
+
+            if (!firePhase && key.name === 'f') {
+                firePhase = true;
             }
+
+            if (['w', 'a', 's', 'd'].includes(key.name)) {
+
+                if (firePhase) {
+                    // fire bullet
+                    game.fireBullet(key.name);
+                    firePhase = false
+                } else {
+                    // try movement
+                    game.tryMoveCharacter(key.name);
+                }
+
+                game.iterateZombies();
+            }
+
             input.removeListener('keypress', listener);
             resolve(str || key.name);
         };
@@ -222,27 +379,44 @@ function waitForKeypress(): Promise<string> {
     });
 }
 
+function repaintTerminal() {
+    const renderViewBox = game.renderViewbox();
+    output.write('\x1b[2J\x1b[0f');
+
+    console.log(`--- ${game.zombiePositions.length} Zombie${game.zombiePositions.length !== 1 ? 's' : ''} Left`);
+    console.log(`--- Mode: ${firePhase ? 'Firing Weapon' : 'Walking'}`);
+    /*
+    console.log(`--- Steps: ${stepCounter} ---`);
+    console.log(`--- Key: ${keyPressed} ---`);
+    console.log(`--- Size: ${game.map.length} x ${game.map[0].length}`);
+    console.log(`--- Position: [${game.characterPosition.row}][${game.characterPosition.col}] ---`);
+    */
+    console.log(renderViewBox);
+
+    if (!(game.gameOver || game.gameWon)) {
+        // game still going
+        console.log(`\n--- CONTROLS --------------` + `\n\n  WASD - Movement` + `\n  F - Fire Weapon + WASD` + `\n\n  Ctrl + C - Quit` + `\n___________________________`);
+
+    } else {
+        // game ended
+        console.log(game.gameWon ? '\n!!! YOU WON !!! (:' : '\n--- GAME OVER :( ---');
+    }
+}
+
 async function runGame() {
-    let counter = 0;
 
     while (true) {
-        // Clear screen and move cursor to top-left
-        const renderViewBox = map.renderViewbox(characterPosition);
-        output.write('\x1b[2J\x1b[0f');
+        repaintTerminal();
 
-        console.log(`--- Steps: ${counter} ---`);
-        console.log(`--- Key: ${keyPressed} ---`);
-        console.log(`--- Size: ${map.map.length} x ${map.map[0].length}`);
-        console.log(`--- Position: [${characterPosition.row}][${characterPosition.col}] ---`);
-        console.log(renderViewBox);
-
-        console.log('\nWASD to move (or Ctrl+C to exit)...');
-
-        // Wait for keypress commands
-        await waitForKeypress();
-
-        // Iterate step
-        counter++;
+        if (!(game.gameOver || game.gameWon)) {
+            // game still going
+            await waitForKeypress();
+            // Iterate step
+            stepCounter++;
+        } else {
+            // game ended
+            process.exit(0);
+        }
     }
 }
 
